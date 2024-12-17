6f1a26d2c98da3fb2607e8843b1a2c4afa84ae3c
Jocelyn <aracely.campos@hotmail.com>
2024-12-17 18:08:59 +0100

    Påbörjat TagsDao

--- src/main/java/org/example/kompletteringsuppgiftajp/DAO/TagsDAO.java
+++ src/main/java/org/example/kompletteringsuppgiftajp/DAO/TagsDAO.java
@@ -1,4 +1,62 @@
 package org.example.kompletteringsuppgiftajp.DAO;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.example.kompletteringsuppgiftajp.entity.Tags;
+
+import java.util.List;
+
 public class TagsDAO {
+
+
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("komplettering.unit");
+
+    //Create
+    private void saveTags(Tags tags) {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+
+        try {
+            transaction = em.getTransaction();
+            transaction.begin();
+            em.persist(tags);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive())  {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+
+    //ReadOne
+    public Tags getTagsId(int id) {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        Tags tags = null;
+
+        try {
+            tags = em.find(Tags.class, id);
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+        finally {
+            em.close();
+        }
+        return tags;
+    }
+
+    //ReadAll
+    public List<Tags> getAllTags () {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        List<Tags> tagsList = null;
+    }
+
+
+    //Update
+    //Delete
+
 }
